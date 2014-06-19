f4b3d4914da4d463d1ad80bb80209baa0731ae83
johnaslorika <zdrojkowski@gmail.com>
2014-06-19 17:55:09 -0500

    Made almost a game
    Added shapes/materials to Unity and made shapes appear on screen and
    move all fancy like.

---
+++ AwesomeProject/Assets/AG_Scripts/MoveShape.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveShape : MonoBehaviour {
+
+	public float moveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.down * Time.deltaTime * moveSpeed;
+	}
+}
---
+++ AwesomeProject/Assets/AG_Scripts/SpawnShapes.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnShapes : MonoBehaviour {
+
+	public GameObject[] spawnObjects;
+	//public Material[] colors;
+	public float spawnTime;
+
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Spawn", spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Spawn() {
+		int i = Random.Range (0, spawnObjects.Length);
+		//int j = Random.Range (0, colors.Length);
+		GameObject shape = (GameObject) Instantiate (spawnObjects [i], transform.position, spawnObjects[i].transform.rotation);
+
+		//shape.renderer.material = colors [j];
+
+		Debug.Log ("SPAWNED ITEM");
+
+		Invoke ("Spawn", spawnTime);
+	}
+	
+}
