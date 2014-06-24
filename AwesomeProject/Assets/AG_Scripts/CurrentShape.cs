4cd415f59b6cf733ba83ea237523ddc211b918a9
johnaslorika <zdrojkowski@gmail.com>
2014-06-24 15:26:56 -0500

    Minor changes of some kind

---
+++ AwesomeProject/Assets/AG_Scripts/CurrentShape.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public float timer = 2.0f;
+
+public class CurrentShape : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float nextTime = 0.0f;
+
+		if(nextTime > timer){
+			// Change Shape
+
+		}
+
+		nextTime += Time.DeltaTime;
+	}
+}
