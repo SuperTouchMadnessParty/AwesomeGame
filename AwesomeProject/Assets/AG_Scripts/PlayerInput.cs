041b97c5734389f6779f11d628f248ac77501af8
johnaslorika <zdrojkowski@gmail.com>
2014-07-05 15:24:21 -0400

    Finished art updates and raycasted the world.

--- AwesomeProject/Assets/AG_Scripts/PlayerInput.cs
+++ AwesomeProject/Assets/AG_Scripts/PlayerInput.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class PlayerInput : MonoBehaviour {
 
+	public GameObject ShapeToClick; 
 	RuntimePlatform platform = Application.platform;
+	public float fTimeToSwap = 10.0f;
+	private float fTimer = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,16 +15,18 @@ public class PlayerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		/*if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer) {
+	
+
+
+		if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer) {
 			// Mobile controls
 			if(Input.touchCount > 0){
 				if(Input.GetTouch(0).phase == TouchPhase.Began){
 					checkTouch(Input.GetTouch(0).position);
 				}
 			}
-		} else*/ if(platform == RuntimePlatform.WindowsPlayer || platform == RuntimePlatform.WindowsEditor){
+		} else if(platform == RuntimePlatform.WindowsPlayer || platform == RuntimePlatform.WindowsEditor){
 
-			//Debug.Log ("OMG");	
 			// Windows controls
 			if(Input.GetMouseButtonDown(0)){
 				checkTouch(Input.mousePosition); 
@@ -31,17 +36,18 @@ public class PlayerInput : MonoBehaviour {
 
 	}
 
-
 	void checkTouch(Vector2 position) {
-		Vector3 worldPoint = Camera.main.ScreenToWorldPoint (position);
-		Vector2 touchPosition = new Vector2 (worldPoint.x, worldPoint.y);
 
-		Collider2D hit = Physics2D.OverlapPoint (touchPosition);
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
 
-		Debug.Log (hit);
-
-		if (hit) {
+		if (Physics.Raycast (ray, out hit)) {
 			Debug.Log (hit.transform.gameObject.name);
+
+			if(hit.transform.gameObject.tag == ShapeToClick.gameObject.tag) {
+				Debug.Log ("YOU DID IT CHAMP!");
+			}
+			Destroy(hit.transform.gameObject);
 		}
 	}
 }
