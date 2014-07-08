ebec73f75b72d3e2ce5f2f15c8d51d1d60663fb9
Scott Boykin <scotteboykin@gmail.com>
2014-07-08 18:08:54 -0500

    Stuff

--- AwesomeProject/Assets/AG_Scripts/PlayerInput.cs
+++ AwesomeProject/Assets/AG_Scripts/PlayerInput.cs
@@ -2,11 +2,8 @@
 using System.Collections;
 
 public class PlayerInput : MonoBehaviour {
-	
-	public GameObject ShapeToClick; 
+
 	RuntimePlatform platform = Application.platform;
-	public float fTimeToSwap = 10.0f;
-	private float fTimer = 0.0f;
 	
 	// Use this for initialization
 	void Start () 
@@ -16,22 +13,6 @@ public class PlayerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer) {
-			// Mobile controls
-			if(Input.touchCount > 0){
-				if(Input.GetTouch(0).phase == TouchPhase.Began){
-					checkTouch(Input.GetTouch(0).position);
-				}
-			}
-		} else if(platform == RuntimePlatform.WindowsPlayer || platform == RuntimePlatform.WindowsEditor){
-			
-			// Windows controls
-			if(Input.GetMouseButtonDown(0))
-			{
-				checkTouch(Input.mousePosition); 
-			}
-		}
 		
 		if (Input.GetKeyDown (KeyCode.Escape)) 
 		{
@@ -46,18 +27,8 @@ public class PlayerInput : MonoBehaviour {
 		
 	}
 	
-	void checkTouch(Vector2 position) {
-		
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-		
-		if (Physics.Raycast (ray, out hit)) {
-			Debug.Log (hit.transform.gameObject.name);
-			
-			if(hit.transform.gameObject.tag == ShapeToClick.gameObject.tag) {
-				Debug.Log ("YOU DID IT CHAMP!");
-			}
-			Destroy(hit.transform.gameObject);
-		}
+	void checkTouch(Vector2 position) 
+	{
+
 	}
 }
