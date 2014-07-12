8c6aa3dbfc5def5e594b4d57977e698a79345594
Scott Boykin <scotteboykin@gmail.com>
2014-07-12 18:40:50 -0500

    Fixed menu shape spawner

--- AwesomeProject/Assets/AG_Scripts/MenuShapeSpawner.cs
+++ AwesomeProject/Assets/AG_Scripts/MenuShapeSpawner.cs
@@ -6,6 +6,9 @@ public class MenuShapeSpawner : MonoBehaviour
 {
 	public List<GameObject> spawnObjects = new List<GameObject>();
 	public float spawnDelay = 1;
+	
+	public Vector3 direction = new Vector3( 0.0f, -1.0f, 0.0f );
+	public float shapeInitialVelocity = 1.75f;
 
 	// Use this for initialization
 	void Start () 
@@ -22,11 +25,11 @@ public class MenuShapeSpawner : MonoBehaviour
 	{
 		int i = Random.Range (0, spawnObjects.Count);
 		//int j = Random.Range (0, colors.Length);
-		GameObject shape = (GameObject) Instantiate (spawnObjects [i], transform.position, spawnObjects[i].transform.rotation);
-
-		//shape.renderer.material = colors [j];
-
-		//Debug.Log ("SPAWNED ITEM"); 
+		GameObject shapeObject = (GameObject)Instantiate (spawnObjects [i], transform.position, spawnObjects[i].transform.rotation);
+		Shape shapeComponent = shapeObject.GetComponent<Shape>();
+		
+		shapeComponent.SetDirection( direction );
+		shapeComponent.SetVelocity( shapeInitialVelocity );
 
 		Invoke ("Spawn", spawnDelay);
 	}
